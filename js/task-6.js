1ef83d472e27acec9b5607542a868aa8f26e52d0
Anastasiya-Shtukar <162809005+Anastasiya-Shtukar@users.noreply.github.com>
2024-08-29 17:56:43 +0200

    task-06

--- js/task-6.js
+++ js/task-6.js
@@ -9,18 +9,23 @@ const buttonCreate = document.querySelector("button[data-create]");
 const controls = document.querySelector("#controls");
 const buttonDestroy = document.querySelector("button[data-destroy]");
 const boxes = document.querySelector("#boxes");
-buttonCreate.addEventListener("click", () => {
-  input.addEventListener("input", (event) => {
-    const currentTargetTrim = event.currentTarget.value.trim();
-    (currentTargetTrim > 1) & (currentTargetTrim < 100);
 
-    const boxesCreate = document.createElement("div");
-    boxesCreate.style.width = "30px";
-    boxesCreate.style.height = "30px";
-    boxesCreate.style.backgroundColor = getRandomHexColor();
-    for (let index = 0; index < currentTargetTrim; index++) {
+buttonCreate.addEventListener("click", () => {
+  boxes.innerHTML = "";
+  if (input.value > 1 && input.value < 100) {
+    for (let index = 1; index <= input.value; index++) {
+      const boxesCreate = document.createElement("div");
       boxes.append(boxesCreate);
+      let x = 20;
+      let sizeBox = x + 10;
+
+      boxesCreate.style.width = `${sizeBox}px`;
+      boxesCreate.style.height = `${sizeBox}px`;
+      boxesCreate.style.backgroundColor = getRandomHexColor();
     }
-    return;
-  });
+  }
+  return (input.value = "");
+});
+buttonDestroy.addEventListener("click", () => {
+  return (boxes.innerHTML = "");
 });
